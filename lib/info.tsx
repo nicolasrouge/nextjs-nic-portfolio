041fcd3b066afe0582bc5f67257a9eb0a49a0bd1
Nicolas Rouge <nicolas.rouge777@gmail.com>
2023-04-07 16:18:23 +0100

    first commit

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,22 +1,29 @@
-import me from '../app/avatar.jpg';
+import me from '../app/placeholder.jpg';
 
-export const name = 'Lee Robinson';
+export const name = 'Your Name';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Lee. I'm the <b>VP of Developer Experience at Vercel</b> where my
-      team helps developers build a faster web.
+      Hey, I'm a developer who loves .NET, React.js and the Cloud.
+      Currently working at Cox Automotive as a Software Engineer 🧑🏻‍💻
+      4+ years of experience in both the UK and France 🇬🇧🇫🇷
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I'm a Next.js contributor and help lead our open-source communities. I'm
-      passionate about frontend development and have created courses on React,
-      Next.js, and web development. I'm an advisor and investor in early stage
-      startups.
+      One of my project from scratch <b>DRiiVE Academy 🎓</b> `Thousands of users at this day` 🚀
+      Clean Architecture, quality code, and a great team are the key to success.
+      I love as well as backend and frontned development, I also enjoy working on side projects.
+      
+      Some Mini project: 
+      - Cox Automotive Hackathon(AI Photobooth) 
+      - 💹 UK IT job market in one glance
+      - 🌎 English Speakers around the world
+      
+      I have a passion for travel and enjoy discovering new destinations. Recently, I tried skiing for the first time and it was an incredible experience. 🗻🚠⛷️
     </>
   );
 };
